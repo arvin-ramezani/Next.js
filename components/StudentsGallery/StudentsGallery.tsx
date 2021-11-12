49bd6e4fdc22a70950cac1c736cddfa9e4f6a189
Arvin <rvin.ramezani@gmail.com>
2021-11-12 21:04:58 +0330

    second commit

--- components/StudentsGallery/StudentsGallery.tsx
+++ components/StudentsGallery/StudentsGallery.tsx
@@ -13,13 +13,13 @@ const StudentsGallery: FC = () => {
       <div className={classes.studentsGalleryText}>
         <h4>Visit Our Student Gallery</h4>
         <p>
-          We are proud of our vibrant student community. NYIP's global reach
+          {`We are proud of our vibrant student community. NYIP's global reach
           means that we are able to bring together photographers from all over
           the world who share a passion for taking great photographs. All of
           NYIP's photography students gain access to a student forum where they
           can share their work, discuss the courses and learn from and offer
           advice to other students. Our student gallery features select examples
-          of great student work. Visit the NYIP Student Photo Gallery.
+          of great student work. Visit the NYIP Student Photo Gallery.`}
         </p>
       </div>
     </section>
